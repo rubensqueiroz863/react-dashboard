e99087348dbf8e6b3a0724a361035715fc78f9b2
rubensqueiroz863 <rubensqueiroz863@gmail.com>
2025-07-30 16:46:09 -0300

    Correção de erro

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,7 +1,7 @@
-export default function() {
-    return (
-        <div>
-            Inicio
-        </div>
-    );
+export default function Home() {
+  return (
+    <div>
+      Inicio
+    </div>
+  );
 }
